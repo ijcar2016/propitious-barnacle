00f9873ed9ad7c5dd7b5ad6a5301ea379bd3d01c
tao-githup <lt732648307@163.com>
2015-10-17 21:35:15 +0800

    Create examples.thy

---
+++ examples.thy
@@ -0,0 +1,26 @@
+theory examples
+imports Main  "basic" "quantum_hoare_logic" "lt"
+begin
+
+lemma grover:"valid I  
+               ((((Init q0 0);Init q1 1;Init q 2;Init r 3;
+               Utrans Delta 2;Utrans H 0;Utrans H 1;Utrans H 2);
+              While M0 M1 C Q);
+          Cond [(N0,SKIP,p0),(N1,SKIP,p1),(N2,SKIP,p2),(N3,SKIP,p3)] )             
+               P"
+apply (vcg,auto)
+apply quantum_oracle
+done
+lemma loop:"valid Q (Utrans Or (10);Utrans H 0;Utrans H 1;Utrans Ph 10;
+         Utrans H 0; Utrans H 1) G"
+apply(vcg,auto)
+apply quantum_oracle
+done
+(*
+lemma shor:"valid P  (Init q0 0;Utrans H 0;Utrans U 10;Utrans (dag U) 10;Utrans (dag H) 0;
+                    Cond [(M0,SKIP,p0),(M1,SKIP,p1)])   Q"
+apply(rule ord_wlp,auto)
+apply quantum_oracle
+done*)
+
+end
